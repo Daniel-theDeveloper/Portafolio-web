4e2a6b6711e6882f1978668d1273b989661ca9c3
Daniel-theDeveloper <danielalfarogamez@gmail.com>
2024-07-12 16:53:07 -0600

    Actualización

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const nav = document.querySelector(".nav");
-const openmenubtn = document.querySelector(".open-menu");
-const closemenubtn = document.querySelector(".close-menu");
+const btnOpenMenu = document.querySelector(".open-menu");
+const btnCloseMenu = document.querySelector(".close-menu");
+
+const btnCV = document.querySelector(".cv");
+
+const email = document.getElementById("email").innerHTML;
+const btnEmailCopy = document.querySelector(".email-copy");
 
 const phone = document.querySelector(".phone");
 const moreInfo = document.querySelector(".more");
@@ -13,7 +18,27 @@ function togglePhone() {
     phone.classList.toggle("phone-straightening");
 }
 
-openmenubtn.addEventListener("click", toggleMenu);
-closemenubtn.addEventListener("click", toggleMenu);
+function viewCV() {
+    window.open("src/docs/Curriculum.pdf", "Curriculum vitae", "fullscreen=yes");
+    parent.opener = top;
+    opener.close();
+}
+
+const copyEmail = async () => {
+    try {
+        await navigator.clipboard.writeText(email);
+        btnEmailCopy.classList.remove("fa-copy");
+        btnEmailCopy.classList.add("fa-check");
+    } catch (err) {
+        alert("Error en la función, detalles: "+err);
+    }
+}
+
+btnOpenMenu.addEventListener("click", toggleMenu);
+btnCloseMenu.addEventListener("click", toggleMenu);
+
+btnCV.addEventListener("click", viewCV);
+
+btnEmailCopy.addEventListener("click", copyEmail);
 
 moreInfo.addEventListener("click", togglePhone);
